90e8fca4f8ded03e938b4ce4e5cdc8fa305aaad0
Muhammad Nawzad <hama127n@gmail.com>
2024-01-21 14:53:05 +0300

    Add configuration and configure method to Inboxable module

--- lib/inboxable.rb
+++ lib/inboxable.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 require_relative 'inboxable/version'
+require_relative 'inboxable/configuration'
+require_relative 'inboxable/polling_receiver_worker'
 
 module Inboxable
   class Error < StandardError; end
-  # Your code goes here...
+
+  attr_accessor :configuration
+
+  def configure
+    @configuration ||= Configuration.new
+    yield(@configuration) if block_given?
+  end
 end
